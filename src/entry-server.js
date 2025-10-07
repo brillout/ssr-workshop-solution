4b556523add03b698b10b35bda47e06b49b11862
Romuald Brillout <git@brillout.com>
2025-10-07 16:35:30 +0200

    add TODOs

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,18 +1,25 @@
-import { renderToString } from 'vue/server-renderer'
-import { createApp } from './main'
+// TODO: Import the renderToString function from Vue's server renderer
+// Hint: import { renderToString } from 'vue/server-renderer'
+
+// TODO: Import the createApp function from './main'
 
 /**
  * @param {string} _url
  */
 export async function render(_url) {
-  const { app } = createApp()
+  // TODO: Create a fresh app instance for this request
+  // Hint: Use the createApp function and destructure to get { app }
+  const { app } = null // Replace this line
 
   // passing SSR context object which will be available via useSSRContext()
   // @vitejs/plugin-vue injects code into a component's setup() that registers
   // itself on ctx.modules. After the render, ctx.modules would contain all the
   // components that have been instantiated during this render call.
   const ctx = {}
-  const html = await renderToString(app, ctx)
+
+  // TODO: Render the app to a string using Vue's server renderer
+  // Hint: Use renderToString(app, ctx)
+  const html = null // Replace this line
 
   return { html }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createSSRApp } from 'vue'
+import { createApp as createVueApp } from 'vue'
+// TODO: Import createSSRApp for SSR compatibility
+// Hint: import { createSSRApp } from 'vue'
+
 import App from './App.vue'
 
 // SSR requires a fresh app instance per request, therefore we export a function
 // that creates a fresh app instance. If using Vuex, we'd also be creating a
 // fresh store here.
 export function createApp() {
-  const app = createSSRApp(App)
+  // TODO: For SSR, replace createVueApp with createSSRApp
+  // This will make the app compatible with server-side rendering
+  const app = createVueApp(App)
   return { app }
 }
