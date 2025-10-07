84ee5b9af9e3c36522f79d9a6f100e167b7df550
Romuald Brillout <git@brillout.com>
2025-10-07 18:40:22 +0200

    add TODOs

--- react/src/entry-client.jsx
+++ react/src/entry-client.jsx
@@ -1,11 +1,19 @@
 import './index.css'
 import { StrictMode } from 'react'
-import { hydrateRoot } from 'react-dom/client'
+// TODO: Import the correct function for SSR hydration
+// Hint: For SSR, use hydrateRoot from 'react-dom/client'
+// For SPA, you would use createRoot from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 
-hydrateRoot(
-  document.getElementById('root'),
+// This works for SPA mode, but for SSR we need hydration
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// TODO: For SSR, replace createRoot().render() with hydrateRoot()
+// Hint: hydrateRoot(document.getElementById('root'), <StrictMode><App /></StrictMode>)
+// The difference: createRoot().render() creates a new DOM tree (SPA)
+// hydrateRoot() attaches to existing server-rendered HTML (SSR)
--- react/src/entry-server.jsx
+++ react/src/entry-server.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
-import { renderToString } from 'react-dom/server'
+// TODO: Import the renderToString function from React DOM server
+// Hint: import { renderToString } from 'react-dom/server'
+
 import App from './App'
 
 /**
  * @param {string} _url
  */
 export function render(_url) {
-  const html = renderToString(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
+  // TODO: Render the React app to a string using React's server renderer
+  // Hint: Use renderToString() with <StrictMode><App /></StrictMode>
+  const html = null // Replace this line
+
   return { html }
 }
